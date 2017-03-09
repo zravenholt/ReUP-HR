08684db6a6967445f3884c19c22e0d56b9e5b26b
David Zou <david.zou@outlook.com>
2017-03-09 11:46:28 -0800

    Created the function to API fetch from IGN. Will need testing.

--- server/api/searchIGN.js
+++ server/api/searchIGN.js
@@ -1,5 +1,8 @@
-// function searchIGN ({key, query, max}, callback) {
-//   return fetch(`https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=${IGN_API_KEY}`);
-  
-
-// }
+function searchIGN (/*{key, query, max}, callback*/) {
+  return fetch(`https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=${IGN_API_KEY}`)
+    .then(function(response) => response.json())
+    .then(function(responseJSON) => console.log(responseJSON))
+    .catch((error) => {
+      console.error(error);
+    });
+}
