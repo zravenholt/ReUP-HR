f13f58aeac86349b6f1ee2ba659889b6dfca5b43
David Zou <david.zou@outlook.com>
2017-03-08 20:47:57 -0800

    Got 'Hello World!' to appear on server.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/../client'));
+
+app.get('/', function(req, res) {
+  res.send('Hello world!');
+});
+
+app.listen(9001, function() {
+  console.log('ReUP: Express server connection established.');
+});
