31f7199dae177a72cc4b28bb34ec965e894712e1
Zane Ravenholt <zravenholt@gmail.com>
2017-03-09 17:06:53 -0800

    Add a search function to allow game access to subreddit

--- server/api/searchReddit.js
+++ server/api/searchReddit.js
@@ -1,4 +1,8 @@
 //send requests to reddit. 
 //gather the top voted posts for the search Query, somehting like 'https://www.reddit.com/r/Overwatch/top/?sort=top&t=week'
 
-let searchReddit = function () {};
+let searchReddit = function (game) {
+  return fetch (`https://www.reddit.com/r/${game}/top/?sort=top&t=day`)
+    .then((data) => data.json())
+    .catch((err) => console.log('error retrieving from Reddit: ', err));
+};
