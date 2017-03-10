90234bfb08857b8f32803a9fab1259e7027e6460
David Zou <david.zou@outlook.com>
2017-03-09 17:39:43 -0800

    Added Giant Bomb GET Request functionality.  Added Twitter's, but that requires OAuth.

--- server/api/searchIGN.js
+++ server/api/searchIGN.js
@@ -1,5 +1,5 @@
 function searchIGN () {
-  return fetch(`https://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey${IGN_API_KEY}`)
+  return fetch(`https://newsapi.org/v1/articles?source=ign&sortBy=latest&apiKey=${IGN_API_KEY}`)
     .then((response) => response.json())
     .then((responseJSON) => console.log(responseJSON))
     .catch((error) => {
