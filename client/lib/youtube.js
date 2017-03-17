b6af91443f2c074baf7f41be2d69cb622032a55d
Daryll <daryllcsh@gmail.com>
2017-03-17 14:20:14 -0700

    Retreive youtube videos from official channels

--- client/lib/youtube.js
+++ client/lib/youtube.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import keys from '../../config/keys.js';
 import React from 'react';
 const game = 'overwatch'; //hardcoded game for now
+const channelId = 'UClOf1XXinvZsy4wKPAkro2A';
 
 const fetchYoutube = (tile) => {
-  axios.get(`https://www.googleapis.com/youtube/v3/search?maxResults=1&part=snippet&q=${game}&type=video&videoCaption=closedCaption&key=${keys.YOUTUBE_API_KEY}`)
+  axios.get(`https://www.googleapis.com/youtube/v3/search?key=${keys.YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`)
     .then((response) => {
       tile.setState ({
         videos: tile.state.videos.concat(response.data.items)
@@ -16,3 +17,6 @@ const fetchYoutube = (tile) => {
 }
 
 export default fetchYoutube;
+
+//https://www.googleapis.com/youtube/v3/search?maxResults=1&part=snippet&q=${game}&type=video&videoCaption=closedCaption&key=${keys.YOUTUBE_API_KEY}`
+
