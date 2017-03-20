ef8873b521ba9afa642582d2251c709fe8c9a950
David Zou <david.zou@outlook.com>
2017-03-19 18:15:30 -0700

    Got navbar navigation to work, still need to work on direct routing via URL and default as Explore.

--- client/index.js
+++ client/index.js
@@ -3,30 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-
-// import {createStore} from 'redux';
-
-// //reducer makes changes to the state based on the action passed to it.
-// //                   the "action" is "dispatched" from where the event takes place
-// const reducer = function(state = {articles: 'default'}, action) {
-//   ignObj.articles = ign.get();
-//   return state;
-// };
-
-// // redux produces a new state within the "store" every time the state is passed through the reducer
-// // the second argument here is the initial state of our "state" (containing all the values of our application state)
-// const store = createStore(allReducers, {});
-
-// // this listens for changes to state so we can console log
-// store.subscribe(() => {
-//   console.log('store has changed ', store.getState());
-// });
-
-//arglebargle
-
-
-
-
-
 //provider tag
 ReactDOM.render(<App />, document.getElementById('app'));
