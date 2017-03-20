172035eb76efe7344fcb8cf74827976e247af782
David Zou <david.zou@outlook.com>
2017-03-20 16:13:48 -0700

    Cleaned up 'Add Game' page with Bootstrap.

--- client/containers/AddGame.jsx
+++ client/containers/AddGame.jsx
@@ -51,7 +51,7 @@ class AddGame extends React.Component {
   }
 
   handleSubmit(event) {
-    event.preventDefault();
+    // event.preventDefault();
     dbPost(this, this.state.gameName, this.state.giantBombName, this.state.subRedditName, this.state.twitchQuery, this.state.youtubeChannelId, this.state.twitterName);
   }
 
@@ -59,7 +59,7 @@ class AddGame extends React.Component {
     if(response.status === 200) {
       console.log('Called redirectToPage successfully with status of 200:', response.data[0].gameName);
       // window.location.href = `/#/${response.data[0].gameName}`;
-      window.location.href = '/#/explore';
+      // window.location.href = '/#/explore';
     } else {
       console.log('Server did not respond back with status code 200:', response.status);
     }
@@ -70,14 +70,40 @@ class AddGame extends React.Component {
     return (
       <div>
         <h1>Please enter game information below!</h1>
-        <form onSubmit={this.handleSubmit}>
-          Game Name: <input type="text" value={this.state.gameName} onChange={this.handleGameNameChange}/><br></br>
-          Giant Bomb Game Name: <input type="text" value={this.state.giantBombName} onChange={this.handleGiantBombNameChange}/><br></br>
-          Subreddit Name: <input type="text" value={this.state.subRedditName} onChange={this.handleSubRedditNameChange}/><br></br>
-          Twitch Query: <input type="text" value={this.state.twitchQuery} onChange={this.handleTwitchQueryChange}/><br></br>
-          Youtube Channel ID: <input type="text" value={this.state.youtubeChannelId} onChange={this.handleYoutubeChannelIdChange}/><br></br>
-          Twitter Handle: <input type="text" value={this.state.twitterName} onChange={this.handleTwitterNameChange}/><br></br>
-          <input type="submit" value="Submit Game" />
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <div className="form-group col-xs-8">
+            <label>Game Name</label><input type="text" className="form-control" value={this.state.gameName} onChange={this.handleGameNameChange} placeholder="i.e. Overwatch" aria-describedby=""/>
+            <span><em>Please type the full game name here. i.e. '<strong>Overwatch</strong>'</em></span>
+          </div>
+          <div className="form-group col-xs-4"></div>
+          <div className="form-group col-xs-8">
+            <label>Giant Bomb Game Name</label><input type="text" className="form-control" value={this.state.giantBombName} onChange={this.handleGiantBombNameChange} placeholder="i.e. overwatch"/>
+            <span><em>i.e. 'overwatch' in http://www.giantbomb.com/<strong>overwatch</strong>/3030-48190/</em></span>
+          </div>
+          <div className="form-group col-xs-4"></div>
+          <div className="form-group col-xs-8">
+            <label>Subreddit Name</label><input type="text" className="form-control" value={this.state.subRedditName} onChange={this.handleSubRedditNameChange} placeholder="i.e. Overwatch"/>
+            <span><em>i.e. 'Overwatch' in https://www.reddit.com/r/<strong>Overwatch</strong>/</em></span>
+          </div>
+          <div className="form-group col-xs-4"></div>
+          <div className="form-group col-xs-8">
+            <label>Twitch Query</label><input type="text" className="form-control" value={this.state.twitchQuery} onChange={this.handleTwitchQueryChange} placeholder="i.e. Overwatch"/>
+            <span><em>i.e. '<strong>Overwatch</strong>' as a search query.</em></span>
+          </div>
+          <div className="form-group col-xs-4"></div>
+          <div className="form-group col-xs-8">
+            <label>Youtube Channel ID</label><input type="text" className="form-control" value={this.state.youtubeChannelId} onChange={this.handleYoutubeChannelIdChange} placeholder="i.e. UClOf1XXinvZsy4wKPAkro2A"/>
+            <span><em>To get the url of a page, copy the string at the end of the Youtube channel URL i.e. https://www.youtube.com/channel/<strong>UClOf1XXinvZsy4wKPAkro2A</strong>.  If the channel uses an alias in the URL, please go to <a href="https://johnnythetank.github.io/youtube-channel-name-converter/">this page</a> to get the channel ID instead.</em></span>
+          </div>
+          <div className="form-group col-xs-4"></div>
+          <div className="form-group col-xs-8">
+            <label>Twitter Handle</label><input type="text" className="form-control" value={this.state.twitterName} onChange={this.handleTwitterNameChange} placeholder="i.e. PlayOverwatch"/>
+            <span><em>i.e. 'PlayOverwatch' in https://twitter.com/<strong>PlayOverwatch</strong>/</em></span>
+          </div>
+          <div className="form-group col-xs-4"></div>
+          <div className="form-group col-xs-8">
+            <input type="submit" className="btn btn-primary" value="Submit Game" />
+          </div>
         </form>
       </div>
     );
